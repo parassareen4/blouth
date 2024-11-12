7a0e7f6b86a429ee17646610ec8860c3b4241aeb
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-12 15:53:39 -0500

    basic auth express

--- social-media-backend/server.js
+++ social-media-backend/server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());    
+app.use(cookieParser());   
+app.use(cors());
+app.use('/auth', authRouter);
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
